a6741f05a475f321e9773031425c6615f5ba54eb
Emine Avşar <mahinurk3438@gmail.com>
2025-04-22 13:34:08 +0300

    Config Dosyası Hazırlandı

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -6,87 +6,38 @@ export const appConfig = {
       "CineTime is your go-to platform for discovering the latest movies, showtimes, and exclusive cinema content. Enjoy seamless ticket booking, personalized recommendations, and a world of entertainment at your fingertips.",
   },
   apiURL: "https://cinetime-api.com",
-  userRightsOnRoutes: [
-    {
-      urlRegex: /\/dashboard$/,
-      roles: ["ADMIN", "MANAGER", "ASSISTANTMANAGER", "TEACHER", "STUDENT"],
+
+  endpoints: {
+    cinema: {
+      list: "/cinemas",
+      details: (id) => `/cinemas/${id}`,
+      halls: (cinemaId) => `/cinemas/${cinemaId}/halls`,
     },
-    { urlRegex: /\/dashboard\/admin$/, roles: ["ADMIN"] },
-    { urlRegex: /\/dashboard\/admin\/new$/, roles: ["ADMIN"] },
-    { urlRegex: /\/dashboard\/manager$/, roles: ["ADMIN"] },
-    { urlRegex: /\/dashboard\/manager\/new$/, roles: ["ADMIN"] },
-    { urlRegex: /\/dashboard\/manager\/\d+$/, roles: ["ADMIN"] },
-    {
-      urlRegex: /\/dashboard\/assistant-manager$/,
-      roles: ["ADMIN", "MANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/assistant-manager\/new$/,
-      roles: ["ADMIN", "MANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/assistant-manager\/\d+$/,
-      roles: ["ADMIN", "MANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/teacher$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/teacher\/new$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/teacher\/\d+$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/lesson$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/lesson\/new$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/education-term$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/education-term\/new$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/program$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/program\/new$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/student$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/student\/new$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    {
-      urlRegex: /\/dashboard\/student\/\d+$/,
-      roles: ["ADMIN", "ASSISTANTMANAGER"],
-    },
-    { urlRegex: /\/dashboard\/student-info$/, roles: ["TEACHER"] },
-    { urlRegex: /\/dashboard\/student-info\/new$/, roles: ["TEACHER"] },
-    { urlRegex: /\/dashboard\/student-info\/\d+$/, roles: ["TEACHER"] },
-    { urlRegex: /\/dashboard\/meet$/, roles: ["TEACHER"] },
-    { urlRegex: /\/dashboard\/meet\/new$/, roles: ["TEACHER"] },
-    { urlRegex: /\/dashboard\/meet\/\d+$/, roles: ["TEACHER"] },
-    {
-      urlRegex: /\/dashboard\/contact-message$/,
-      roles: ["ADMIN", "MANAGER", "ASSISTANTMANAGER"],
-    },
-    { urlRegex: /\/dashboard\/choose-lesson$/, roles: ["STUDENT"] },
-    { urlRegex: /\/dashboard\/grades-meets$/, roles: ["STUDENT"] },
-  ],
+  },
+  movie: {
+    list: "/movies",
+    create: "/movies",
+    showtimes: (movieId) => `/movies/${movieId}/show-times`,
+    bySlug: (slug) => `/movies/${slug}`,
+    comingSoon: "/movies/coming-soon",
+    byHall: (hallName) => `/movies/hall/${hallName}`,
+    inTheaters: "/movies/in-theaters",
+  },
+  user: {
+    login: "/login",
+    register: "/register",
+    forgotPassword: "/forgot-password",
+    resetPassword: "/reset-password",
+    validateResetCode: "/validate-reset-password-code",
+    search: "/users/admin",
+    create: "/users/auth",
+    update: "/users/auth",
+    delete: "/users/auth",
+  },
+  favorites: {
+    userFavorites: "/favorites/auth",
+  },
+  hall: {
+    specialHalls: "/halls/special-halls",
+  },
 };
