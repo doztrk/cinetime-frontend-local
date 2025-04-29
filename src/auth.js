70a5b112a26dee40c708eb34f7a8fec18fd8f4b1
Arda Baharoğlu <arda.900bhrgl@gmail.com>
2025-04-29 17:00:48 +0300

    auth.js

--- src/auth.js
+++ src/auth.js
@@ -18,41 +18,27 @@ const config = {
         };
 
         const res = await login(loginData);
+        const data = await res.json();
         if (!res.ok) {
           return null;
         }
 
-        const data = await res.json();
-        const accessToken = data.token;
-        if (!accessToken) {
-          return null;
-        }
-
-        // Decode etmek sadece token süresini kontrol etmek için işine yarayacak
-        const decoded = parseJWT(accessToken);
-
-        return {
-          user: {
-            phoneNumber: decoded?.sub || credentials.phoneNumber,
-            role: "user", // Çünkü token içinde rol yok
-          },
-          accessToken: accessToken,
+        const payload = {
+          user: { ...data },
+          accessToken: data.token,
         };
+        delete payload.user.token;
+
+        return payload;
       },
     }),
   ],
   callbacks: {
     async jwt({ token, user }) {
-      // Eğer yeni login olmuşsa user objesi doludur
+      console.log("TOKEN:", token);
       if (user) {
-        token.phoneNumber = user.phoneNumber ?? user.sub; // phoneNumber varsa al, yoksa sub'dan al
-        token.role = user.role || "user";
-        token.accessToken = user.accessToken;
-      } else if (token.sub && !token.phoneNumber) {
-        // Refresh gibi durumlarda sub'ı phoneNumber olarak kullan
-        token.phoneNumber = token.sub;
+        return { ...token, ...user };
       }
-
       return token;
     },
 
@@ -60,7 +46,6 @@ const config = {
       console.log("Session Callback - Token:", token);
 
       const { accessToken, phoneNumber, role } = token;
-
       const isAPITokenValid = getIsTokenValid(accessToken);
       if (!isAPITokenValid) {
         console.log("Token geçersiz.");
